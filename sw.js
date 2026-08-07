cd01d5a9cb5037397e3d1ed087c5a01275247604
skytins3-png <skytins3@gmail.com>
2026-08-08 07:09:04 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='maru-music-v0.17.6';
-const FILES=['./','index.html','styles.css?v=0.17.6','score.css?v=0.17.6','vendor/vexflow.js?v=5.0.0','vendor/sfumato.js?v=0.17.6','samples/GENERALUSER-LICENSE.txt','vendor/SOUNDFONT2-ESM-LICENSE.txt','vendor/SFUMATO-LICENSE.txt','app.js?v=0.17.6','manifest.webmanifest','icons/icon.svg'];
+const CACHE='maru-music-v0.17.7';
+const FILES=['./','index.html','styles.css?v=0.17.7','score.css?v=0.17.7','vendor/vexflow.js?v=5.0.0','vendor/sfumato.js?v=0.17.7','samples/GENERALUSER-LICENSE.txt','vendor/SOUNDFONT2-ESM-LICENSE.txt','vendor/SFUMATO-LICENSE.txt','app.js?v=0.17.7','manifest.webmanifest','icons/icon.svg'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)))});
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{
