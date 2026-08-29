2b80f4623fed58789f839cab4f0c233301078369
skytins3-png <skytins3@gmail.com>
2026-08-30 06:22:16 +0900

    Add files via upload

--- PC-OBS-HELPER/START-MARU-OBS-HELPER.bat
+++ PC-OBS-HELPER/START-MARU-OBS-HELPER.bat
@@ -1,14 +1,14 @@
 @echo off
 setlocal
 cd /d "%~dp0"
-title MARU OBS Helper Launcher V0.22.93
+title MARU OBS Helper Launcher V0.22.96
 where powershell.exe >nul 2>nul
 if errorlevel 1 (
   echo PowerShell was not found.
   pause
   exit /b 1
 )
-start "MARU OBS Helper V0.22.93" powershell.exe -NoLogo -NoProfile -ExecutionPolicy Bypass -File "%~dp0MARU-OBS-Helper.ps1"
+start "MARU OBS Helper V0.22.96" powershell.exe -NoLogo -NoProfile -ExecutionPolicy Bypass -File "%~dp0MARU-OBS-Helper.ps1"
 timeout /t 2 /nobreak >nul
 start "" "http://127.0.0.1:8765/"
 exit /b 0
